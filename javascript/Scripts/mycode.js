ef548cee803787ddcad8fc9db12698db52945df7
jspractice-1480122229 <jspractice-1480122229@mailforger.com>
2016-12-03 22:05:52 -0800

    commit to save recent changes

--- javascript/Scripts/mycode.js
+++ javascript/Scripts/mycode.js
@@ -1,17 +1,17 @@
 var mydate = new Date();
-document.write("Dynamically created: " + mydate.toDateString() + " " + mydate.toTimeString() + "<br />");
-mydate.setDate(mydate.getDate() + 33); // add 33 days to the 'date' part
-document.write("After adding 33 days: " + mydate.toDateString() + " " + mydate.toTimeString());
+// document.write("Dynamically created: " + mydate.toDateString() + " " + mydate.toTimeString() + "<br />");
+// mydate.setDate(mydate.getDate() + 33); // add 33 days to the 'date' part
+// document.write("After adding 33 days: " + mydate.toDateString() + " " + mydate.toTimeString());
 // alert("Right now, it's " + mydate + ", ya know?");
 var year = mydate.getFullYear();
 // alert("The year is " + year);
 // window.alert("Here is my message");
-document.write("<h1>Here is another message for you.</h1><p>New paragraph.</p>");
+// document.write("<h1>Here is another message for you.</h1><p>New paragraph.</p>");
 //window.alert(document.title);
-document.write(15 / 8);
-document.write("<p>Here is yet <em>another</em> message for you.</h1><p><em>Another</em> new paragraph.</p>");
-document.write(15 % 6);
-document.title = "NO TITLES";
+// document.write(15 / 8);
+// document.write("<p>Here is yet <em>another</em> message for you.</h1><p><em>Another</em> new paragraph.</p>");
+// document.write(15 % 6);
+// document.title = "NO TITLES";
 function button() {
     alert("So you clicked, eh?");
 };
