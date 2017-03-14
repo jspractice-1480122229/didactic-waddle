cc05212090aa4bde95f9bfe85b55b4d7d28abae6
jspractice-1480122229 <jspractice-1480122229@mailforger.com>
2017-03-14 16:23:34 -0700

    streamline perlpie

---
+++ perlpie/scripts/mailto.js
@@ -0,0 +1,6 @@
+var jhngjhvbh = 'MailForGer'
+var uoklnmmkn = '.com'
+var eaqsxwrfd = 'perlpie'
+var slakj = eaqsxwrfd + '@' + jhngjhvbh + uoklnmmkn
+var message = 'Contact'
+document.write('<a href=\'mailto:' + slakj + '\'>' + message + '</a>')
