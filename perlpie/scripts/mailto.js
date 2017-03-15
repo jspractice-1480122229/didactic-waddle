713c4730d577d34d3990898cde09eb8cfef872b8
jspractice-1480122229 <jspractice-1480122229@mailforger.com>
2017-03-14 18:46:01 -0700

    finally perlpie!

--- perlpie/scripts/mailto.js
+++ perlpie/scripts/mailto.js
@@ -2,5 +2,5 @@ var jhngjhvbh = 'MailForGer'
 var uoklnmmkn = '.com'
 var eaqsxwrfd = 'perlpie'
 var slakj = eaqsxwrfd + '@' + jhngjhvbh + uoklnmmkn
-var message = 'Contact'
+var message = 'Contact ME, not the original author(s)'
 document.write('<a href=\'mailto:' + slakj + '\'>' + message + '</a>')
