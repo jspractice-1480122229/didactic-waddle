630825c977db85f4e493c4a04915b9586f236497
jspractice-1480122229 <jspractice-1480122229@mailforger.com>
2019-05-27 18:03:20 -0700

    re-styling, renaming

--- MailForGer.com/scripts/swapper.js
+++ MailForGer.com/scripts/swapper.js
@@ -1,10 +1,10 @@
 var rando = Math.round(Math.random());
 if (rando === 0) {
   document.open();
-  document.writeln('  <link rel="stylesheet" type="text/css" href="css/site.css">');
+  document.writeln('  <link rel="stylesheet" type="text/css" href="css/site0.css">');
   document.close();
 } else if (rando === 1) {
   document.open();
-  document.writeln('  <link rel="stylesheet" type="text/css" href="css/local-style.css">');
+  document.writeln('  <link rel="stylesheet" type="text/css" href="css/site1.css">');
   document.close();
 }
