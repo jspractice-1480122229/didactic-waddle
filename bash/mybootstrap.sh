e8b83e6563ebbf3e1ef5d1a0fa762a697ed863bb
jspractice-1480122229 <jspractice-1480122229@MailForGer.com>
2021-09-30 21:08:44 -0700

    Updated approach.

--- bash/mybootstrap.sh
+++ bash/mybootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## bootstrap for new nodes with BASH
 echo "My bootstrap begins..."
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove
+sudo apt update && sudo apt upgrade -y && sudo apt -y autoremove
 sudo apt install -y curl wget git
 mv -fv "${HOME}"/.bashrc dotbashrc
 wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bashrc
@@ -10,10 +10,9 @@ wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/mas
 source "${HOME}"/.bashrc
 git clone --depth=1 https://github.com/amix/vimrc.git "${HOME}"/.vim_runtime
 sh "${HOME}"/.vim_runtime/install_awesome_vimrc.sh
-cd "${HOME}"/.vim_runtime
 echo "Setup Vundle for YouCompleteMe"
 git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim #Vundle is for YCM
-echo 'set nocompatible
+echo "set nocompatible
 filetype off
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
@@ -26,7 +25,8 @@ set number
 colorscheme elflord
 set t_Co=256
 
-if has("gui_running")
+if has(\"gui_running\")
     set guifont=DejaVu\ Sans\ Mono\ 12
-endif' > "${HOME}"/.vim_runtime/my_configs.vim
-echo "My bootstrap is done"
+endif" > "${HOME}"/.vim_runtime/my_configs.vim
+echo "My bootstrap is done.
+--> DO NOT FORGET TO 'source ~/.bashrc' <--"
