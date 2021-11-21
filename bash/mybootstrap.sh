1b22c92e8aec3b6d31c45ebceec012de56faf7dc
jspractice-1480122229 <jspractice-1480122229@>
2021-11-21 15:29:04 -0800

    nuke .vimrc; update nodejs to manual install. ~g

--- bash/mybootstrap.sh
+++ bash/mybootstrap.sh
@@ -2,14 +2,23 @@
 ## bootstrap for new nodes with BASH
 echo "My bootstrap begins..."
 sudo apt update
-sudo apt install -y wget git cmake tree colordiff net-tools aptitude
+sudo apt install -y wget curl git cmake tree colordiff net-tools aptitude
 mv -fv "${HOME}"/.bashrc dotbashrc
 wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bashrc
 wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_aliases
 wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_functions
 curl -o "${HOME}"/.gitignore_global.txt https://raw.githubusercontent.com/padosoft/gitignore/master/gitignore_global.txt
 #wget https://raw.githubusercontent.com/padosoft/gitignore/master/gitignore_global.txt -O "${HOME}"/.gitignore_global.txt
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+## MANUAL INSTALL OF nodejs, per https://github.com/nodesource/distributions/blob/master/README.md#debmanual
+KEYRING=/usr/share/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
+# wget can also be used:
+# wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
+gpg --no-default-keyring --keyring "$KEYRING" --list-keys
+VERSION=node_16.x
+DISTRO="$(lsb_release -s -c)"
+echo "deb [signed-by=$KEYRING] https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=$KEYRING] https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt update && sudo apt upgrade -y && sudo apt -y autoremove --purge
 git clone --depth=1 https://github.com/amix/vimrc.git "${HOME}"/.vim_runtime
 sh "${HOME}"/.vim_runtime/install_awesome_vimrc.sh
