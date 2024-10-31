642f73a2cb14c5c1457b21ad70614f2e4604d6e4
Gerry Araujo <gerry.araujo@aurachat.ai>
2024-10-31 13:04:11 -0700

    update 2025410311303

--- bash/mybootstrap.sh
+++ bash/mybootstrap.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 ## bootstrap for new nodes with BASH
 echo "My bootstrap begins..."
-sudo apt update
-sudo apt install -y wget curl git cmake tree colordiff net-tools aptitude
-mv -fv "${HOME}"/.bashrc dotbashrc
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bashrc
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_aliases
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_functions
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.gitconfig
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.git-credentials
-wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/symbols.txt
-curl -o "${HOME}"/.gitignore_global.txt https://raw.githubusercontent.com/padosoft/gitignore/master/gitignore_global.txt
-#wget https://raw.githubusercontent.com/padosoft/gitignore/master/gitignore_global.txt -O "${HOME}"/.gitignore_global.txt
-## MANUAL INSTALL OF nodejs, per https://github.com/nodesource/distributions/blob/master/README.md#debmanual
-#Use fnm from https://github.com/Schniz/fnm
+
+# Update and install essential packages
+sudo apt update && sudo apt install -y wget curl git cmake tree colordiff net-tools yt-dlp ffmpeg lame ghostscript perl webp fortune gawk sed uuid-runtime imagemagick jpegoptim openssl tar unzip unrar-free p7zip-full make gcc cmark ghostwriter screenfetch
+
+# Backup existing .bashrc
+mv -fv "${HOME}/.bashrc" "${HOME}/dotbashrc"
+
+# Download new Bash configurations
+wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bashrc -O "${HOME}/.bashrc"
+wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_aliases -O "${HOME}/.bash_aliases"
+wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.bash_functions -O "${HOME}/.bash_functions"
+wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.gitconfig -O "${HOME}/.gitconfig"
+wget https://raw.githubusercontent.com/jspractice-1480122229/didactic-waddle/master/bash/.git-credentials -O "${HOME}/.git-credentials"
+curl -o "${HOME}/.gitignore_global.txt" https://raw.githubusercontent.com/padosoft/gitignore/master/gitignore_global.txt
+
+# Install fnm (Fast Node Manager) and Node.js
+# Use fnm from https://github.com/Schniz/fnm
 curl -fsSL https://fnm.vercel.app/install | bash
-fnm install v20.11.0
-sudo apt update && sudo apt upgrade -y && sudo apt -y autoremove --purge
-git clone --depth=1 https://github.com/amix/vimrc.git "${HOME}"/.vim_runtime
-sh "${HOME}"/.vim_runtime/install_awesome_vimrc.sh
+
+# Update and clean up packages
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y --purge
+
+# Install Vim and the awesome vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git "${HOME}/.vim_runtime"
+sh "${HOME}/.vim_runtime/install_awesome_vimrc.sh"
+
 echo "Setup Vundle for YouCompleteMe"
-#Vundle is for YCM
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim
-echo "set nocompatible
+# Setup Vundle for YouCompleteMe
+git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+
+# Configure Vim plugins and settings
+cat <<EOL > "${HOME}/.vim_runtime/my_configs.vim"
+set nocompatible
 filetype off
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
@@ -36,8 +47,11 @@ colorscheme slate
 set t_Co=256
 set encoding=utf-8
 
-if has(\"gui_running\")
-    set guifont=Source\ Code\ Pro\ 12
-endif" >"${HOME}"/.vim_runtime/my_configs.vim
-echo "My bootstrap is done.
---> DO NOT FORGET TO 'source ~/.bashrc' <--"
+if has("gui_running")
+    set guifont=Ubuntu\ Mono\ 13
+endif
+EOL
+
+# Notify the user to source .bashrc
+echo "My bootstrap is done."
+echo "--> DO NOT FORGET TO 'source ~/.bashrc' <--"
