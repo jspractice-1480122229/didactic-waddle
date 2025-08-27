fec9194a62a7a6705ebd5d59362f264e35750081
Margi Naverra <23748330+jspractice-1480122229@users.noreply.github.com>
2025-08-27 11:53:14 -0700

    Create config.personal.fish

---
+++ fish/config.personal.fish
@@ -0,0 +1,17 @@
+# FILE: ~/.config/fish/config.personal.fish
+# Sourced by config.fish. Put your personal functions and variables here.
+
+# -- ALIASES --
+# alias my-server 'ssh user@my-server.com'
+# alias aye 'cd ~/projects/my-project; and podman-compose up -d'
+
+# -- ENVIRONMENT VARIABLES (set -Ux makes them universal and exported) --
+# Be careful with committing secrets to version control!
+# set -Ux OPENAI_API_KEY "sk-..."
+# set -Ux GITHUB_TOKEN "ghp_..."
+
+# -- FUNCTIONS --
+# function backup_database
+#     echo "Backing up my_database..."
+#     pg_dump -U myuser -h localhost my_database > "$HOME/backups/db_(date +%F).sql"
+# end
