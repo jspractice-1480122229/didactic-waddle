288c14550d94cda66aba05a3ace90a61c8588797
Margi Naverra <23748330+jspractice-1480122229@users.noreply.github.com>
2026-04-26 13:36:53 -0700

    Delete MailForGer.com directory
    Moved out as its own project

--- MailForGer.com/perlpie/scripts/mailto.js
+++
@@ -1,6 +0,0 @@
-var jhngjhvbh = 'MailForGer'
-var uoklnmmkn = '.com'
-var eaqsxwrfd = 'perlpie'
-var slakj = eaqsxwrfd + '@' + jhngjhvbh + uoklnmmkn
-var message = 'Contact ME, not the original author(s)'
-document.write('<a href=\'mailto:' + slakj + '\'>' + message + '</a>')
